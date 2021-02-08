fa8ca688c3f266b095f4c58631ad6a20074688f3
Nozicov <yevgeniy.nozikov@aitupay.kz>
2021-02-09 04:40:42 +0600

    Add classes test off lesson

---
+++ sandbox/src/main/java/kz/nee/sandbox/Square.java
@@ -0,0 +1,14 @@
+package kz.nee.sandbox;
+
+public class Square {
+  public double l;
+
+  public Square(double l){
+    this.l = l;
+  }
+
+  public double area(){
+    return this.l * this.l;
+  }
+
+}
