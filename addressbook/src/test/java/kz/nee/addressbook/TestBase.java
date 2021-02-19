75fab38ace1d8693c116230f2e0dccd6a55a6bbf
Nozicov <yevgeniy.nozikov@aitupay.kz>
2021-02-20 02:27:08 +0600

    Refactor ContactCreationTest (extends)

--- addressbook/src/test/java/kz/nee/addressbook/TestBase.java
+++ addressbook/src/test/java/kz/nee/addressbook/TestBase.java
@@ -61,6 +61,39 @@ public class TestBase {
     wd.findElement(By.linkText("groups")).click();
   }
 
+  protected void gotoHomePage() {
+    wd.findElement(By.linkText("home page")).click();
+  }
+
+  protected void submitContactCreation() {
+    wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
+  }
+
+  protected void fillContactForm(ContactData contactData) {
+    wd.findElement(By.name("firstname")).click();
+    wd.findElement(By.name("firstname")).clear();
+    wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
+    wd.findElement(By.name("lastname")).click();
+    wd.findElement(By.name("lastname")).clear();
+    wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
+    wd.findElement(By.name("nickname")).click();
+    wd.findElement(By.name("nickname")).clear();
+    wd.findElement(By.name("nickname")).sendKeys(contactData.getNickname());
+    wd.findElement(By.name("home")).click();
+    wd.findElement(By.name("home")).clear();
+    wd.findElement(By.name("home")).sendKeys(contactData.getHome());
+    wd.findElement(By.name("mobile")).click();
+    wd.findElement(By.name("mobile")).clear();
+    wd.findElement(By.name("mobile")).sendKeys(contactData.getMobile());
+    wd.findElement(By.name("email")).click();
+    wd.findElement(By.name("email")).clear();
+    wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
+  }
+
+  protected void gotoCreationContactPage() {
+    wd.findElement(By.linkText("add new")).click();
+  }
+
   @AfterMethod(alwaysRun = true)
   public void tearDown() throws Exception {
     wd.quit();
