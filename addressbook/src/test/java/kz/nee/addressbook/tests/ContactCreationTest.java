9a70dd9ffc125115f93169b343c5d8a0c89676d2
Nozicov <yevgeniy.nozikov@aitupay.kz>
2021-03-02 02:26:43 +0600

    Optimized test in review

--- addressbook/src/test/java/kz/nee/addressbook/tests/ContactCreationTest.java
+++ addressbook/src/test/java/kz/nee/addressbook/tests/ContactCreationTest.java
@@ -12,19 +12,20 @@ public class ContactCreationTest extends TestBase {
   @Test
   public void testContactCreation() throws Exception {
     app.getNavigationHelper().gotoHomePage();
-    List<ContactData> before = app.getContactHelper().getContactList();
     ContactData contact = new ContactData("Yevgeniy", "Nozikov", "NEE", null, "+77075555555", "nee@nee.kz", "Group1");
+
+    List<ContactData> before = app.getContactHelper().getContactList();
+
     app.getContactHelper().createContact(contact, true);
-    app.getNavigationHelper().gotoHomePage();
+    app.getContactHelper().gotoContactPage();
+
     List<ContactData> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(), before.size() + 1);
 
-    //int max = after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId();
     int max = after.stream().max(Comparator.comparingInt(ContactData::getId)).get().getId();
     contact.setId(max);
     before.add(contact);
 
-    //Comparator<? super ContactData> byId = (c1, c2) -> Integer.compare(c1.getId(), c2.getId());
     Comparator<? super ContactData> byId = Comparator.comparingInt(ContactData::getId);
     before.sort(byId);
     after.sort(byId);
